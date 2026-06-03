8fa04f7e69dcecb61d662a7f77eea43dfcddf11e
Rahan-M <rahan01729@gmail.com>
2026-06-03 07:03:29 +0530

    stage1

--- NITCbase/mynitcbase/main.cpp
+++ NITCbase/mynitcbase/main.cpp
@@ -2,6 +2,8 @@
 #include "Cache/OpenRelTable.h"
 #include "Disk_Class/Disk.h"
 #include "FrontendInterface/FrontendInterface.h"
+#include <iostream>
+#include <cstring>
 
 int main(int argc, char *argv[]) {
   /* Initialize the Run Copy of Disk */
@@ -9,5 +11,18 @@ int main(int argc, char *argv[]) {
   // StaticBuffer buffer;
   // OpenRelTable cache;
 
-  return FrontendInterface::handleFrontend(argc, argv);
+  // Disk functions expects 2 arguments, 1 buffer of size 2048 bytes and block number
+
+  unsigned char buffer[2048];
+  // We use unsigned char as it is of size 1 bytes,
+  // This accurately simulates one block
+
+  Disk::readBlock(buffer, 0);
+  // 7000 is a random unused block number
+
+  for(int i=0;i<10;i++){
+    std::cout<<int(buffer[i])<<'\n';
+  }
+  // return FrontendInterface::handleFrontend(argc, argv);
+  return 0;
 }
