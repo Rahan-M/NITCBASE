b864ffd465c14e1e5292a970aa8eb75455d8a624
Rahan-M <rahan01729@gmail.com>
2026-06-13 18:40:13 +0530

    feat(RelCacheTable): Add setRelCatEntry method and relCatEntryToRecord method

--- NITCbase/mynitcbase/Cache/RelCacheTable.cpp
+++ NITCbase/mynitcbase/Cache/RelCacheTable.cpp
@@ -38,6 +38,15 @@ void RelCacheTable::recordToRelCatEntry(union Attribute record[RELCAT_NO_ATTRS],
     relCatEntry->numSlotsPerBlk=(int)record[RELCAT_NO_SLOTS_PER_BLOCK_INDEX].nVal;
 }
 
+void RelCacheTable::relCatEntryToRecord(RelCatEntry *relCatEntry, Attribute record[RELCAT_NO_ATTRS]){
+    strcpy(record[RELCAT_REL_NAME_INDEX].sVal, relCatEntry->relName);
+    record[RELCAT_NO_ATTRIBUTES_INDEX].nVal=relCatEntry->numAttrs;
+    record[RELCAT_NO_RECORDS_INDEX].nVal=relCatEntry->numRecs;
+    record[RELCAT_NO_SLOTS_PER_BLOCK_INDEX].nVal=relCatEntry->numSlotsPerBlk;
+    record[RELCAT_FIRST_BLOCK_INDEX].nVal=relCatEntry->firstBlk;
+    record[RELCAT_LAST_BLOCK_INDEX].nVal=relCatEntry->lastBlk;
+}
+
 
 
 /* will return the searchIndex for the relation corresponding to `relId
@@ -71,4 +80,23 @@ int RelCacheTable::setSearchIndex(int relId, RecId* searchIndex){
 int RelCacheTable::resetSearchIndex(int relId){
     RecId initVal={-1, -1};
     return setSearchIndex(relId, &initVal);
+}
+
+int RelCacheTable::setRelCatEntry(int relId, RelCatEntry *relCatBuf) {
+    if(relId<0 || relId>MAX_OPEN)
+        return E_OUTOFBOUND;
+
+    // if there's no entry at the rel-id
+    if(relCache[relId]==nullptr)
+        return E_RELNOTOPEN;
+
+    // copy the relCatBuf to the corresponding Relation Catalog entry in
+    // the Relation Cache Table.
+    relCache[relId]->relCatEntry=*relCatBuf;
+
+    // set the dirty flag of the corresponding Relation Cache entry in
+    // the Relation Cache Table.
+    relCache[relId]->dirty=true;
+
+    return SUCCESS;
 }
